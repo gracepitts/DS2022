884534223fb2f95bc8422dcaab71d5f5aa02519c
twg3sr <157422808+gracepitts@users.noreply.github.com>
2024-09-22 20:34:13 +0000

    I made a madlib

---
+++ my-work/lab3/madlib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an animal: " NOUN1
+read -p "2. Name a place: " NOUN2
+read -p "3. Name an negative personality trait : " ADJECTIVE1
+read -p "4. Name a celebration: " VERB1
+read -p "5. Name a type of weather: " NOUN3
+read -p "6. Name a number: " NOUN4
+read -p "7. Name a food: " NOUN5
+read -p "8. Name a different animal from before: " NOUN6
+
+
+echo "Once upon a time there was a $NOUN1. He dreamed of going to $NOUN2 but he was too $ADJECTIVE1 to go. For his fifth birthday, his mom told him
+ he gets to go to $NOUN2. He $VERB1 in celebration. On the day he was supposed to go to $NOUN2 it was $NOUN3 so his mom said he couldn't go. He 
+was devasted. He cried for $NOUN4 days straight. Since he was so sad, his mom gave him $NOUN5. That made him happy temporarily, but he still really
+ wanted to go to $NOUN2. He was so mad at his mom for not taking him to $NOUN2 that he gave her the silent treatment. Nothing would make him happy
+besides going to $NOUN2. Weeks later, he met a $NOUN6. $NOUN6  and him ran away and finally went to $NOUN2 and he was finally happy again. 
+The end."
