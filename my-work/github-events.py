79f2465ced70f234e392b100d7ab73605a2a6865
twg3sr <157422808+gracepitts@users.noreply.github.com>
2024-09-22 21:48:59 +0000

    finished editing fetch data

--- my-work/github-events.py
+++ my-work/github-events.py
@@ -3,20 +3,13 @@
 import os
 import json
 import requests
-
-# Step 1: Retrieve GitHub username from environment variable
 GHUSER = os.getenv('GITHUB_USER')
 
-# Step 2: Define the API URL dynamically using the username
 url = 'https://api.github.com/users/{0}/events'.format(GHUSER)
 
-# Step 3: Fetch the recent GitHub activity for the user
 response = requests.get(url)
+r = json.loads(response.text)
 
-# Step 4: Parse the JSON response
-events = json.loads(response.text)
-
-# Step 5: Loop through the first 5 events and print them
-for event in events[:5]:
-    event_info = event['type'] + ' :: ' + event['repo']['name']
-    print(event_info)
+for x in r[:5]:
+    event = x['type'] + ' :: ' + x['repo']['name']
+    print(event)
